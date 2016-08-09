0633fbac9d0f9a749b27c7ae10d079f37da58a50
Aman Mittal <amandeepmittal@live.com>
2016-08-10 00:51:04 +0530

    chore(releasing): add travis config, conventional commit, and semantic-release
    closes #1

--- test/startrek.test.js
+++ test/startrek.test.js
@@ -1,8 +1,31 @@
-var starTrekNames = require("../lib/startrek");
-var expect = require("chai").expect;
+var startrek = require('../lib/startrek');
+var expect = require('chai').expect;
 
 describe('startrek-names', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(startrek.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings (arr) {
+        return arr.every(function (element) {
+          return typeof element === 'string';
+        });
+      }
+    });
+
+    it('should contain one vulcan ', function () {
+      expect(startrek.all).to.include('Spock');
+    });
+
+    it('should contain captain James T. Kirk', function () {
+      expect(startrek.all).to.include('James T. Kirk');
+    });
+  });
+
+  describe('random names', function () {
+    it('should return a random startrek character name', function () {
+      var randomName = startrek.random();
+      expect(startrek.all).to.include(randomName);
+    });
   });
 });
