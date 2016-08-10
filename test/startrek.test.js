989ac486aafc9821f56803bc1add8f2c7fe6ab61
Aman Mittal <amandeepmittal@live.com>
2016-08-10 12:15:52 +0530

    perf(Code and Tests in ES6):

--- test/startrek.test.js
+++ test/startrek.test.js
@@ -1,37 +1,37 @@
 import startrek from '../lib/startrek';
 import {expect} from 'chai';
 
-describe('startrek-names', function () {
-  describe('all', function () {
-    it('should be an array of strings', function () {
+describe('startrek-names', () => {
+  describe('all', () => {
+    it('should be an array of strings', () => {
       expect(startrek.all).to.satisfy(isArrayOfStrings);
 
       function isArrayOfStrings (arr) {
-        return arr.every(function (element) {
+        return arr.every((element) => {
           return typeof element === 'string';
         });
       }
     });
 
-    it('should contain one vulcan ', function () {
+    it('should contain one vulcan ', () => {
       expect(startrek.all).to.include('Spock');
     });
 
-    it('should contain captain James T. Kirk', function () {
+    it('should contain captain James T. Kirk', () => {
       expect(startrek.all).to.include('James T. Kirk');
     });
   });
 
-  describe('random names', function () {
-    it('should return a random startrek character name', function () {
+  describe('random names', () => {
+    it('should return a random startrek character name', () => {
       var randomName = startrek.random();
       expect(startrek.all).to.include(randomName);
     });
 
-    it('should return an array of random names if passed a number', function() {
+    it('should return an array of random names if passed a number', () => {
       var randomNames = startrek.random(3);
       expect(randomNames).to.have.length(3);
-      randomNames.forEach(function(element) {
+      randomNames.forEach((element) => {
         expect(startrek.all).to.include(element);
       });
     });
